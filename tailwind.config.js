318ccb4fc2dc4379156b93ddfefe9086e5eeff17
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-08-24 18:25:42 +0700

    feat : adjust hero responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
+    colors: {
+      'jess': {
+        "100": "#E1F4EC",
+        "200": "#B3E4D1",
+        "300": "#80D2B4",
+        "400": "#4CCF97",
+        "DEFAULT": "#20B486",
+        "600": "#1A946D",
+        "700": "#137653",
+        "800": "#0C573A",
+        "900": "#063924"
+      }
+    }
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
@@ -13,6 +26,6 @@ export default {
     themes: [
       "nord"
     ]
-  },
+  }
 }
 
