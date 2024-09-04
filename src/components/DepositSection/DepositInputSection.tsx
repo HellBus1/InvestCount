0d008ce838bf491285198e918988da1b353eefd0
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-09-04 08:27:50 +0700

    feat : adjust deposit type wording, working deposit calculation interest algorithm

--- src/components/DepositSection/DepositInputSection.tsx
+++ src/components/DepositSection/DepositInputSection.tsx
@@ -2,65 +2,137 @@ import { useState } from 'react'
 
 const DepositInputSection = () => {
   const [amount, setAmount] = useState('')
+  const [interestRate, setInterestRate] = useState('')
+  const [taxRate, setTaxRate] = useState('')
+  const [holdingMonths, setHoldingMonths] = useState('')
   const [interest, setInterest] = useState('')
-  const [years, setYears] = useState('')
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(e.target.value)
+  }
+
+  const handleInterestRateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInterestRate(e.target.value)
+  }
+
+  const handleTaxRateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaxRate(e.target.value)
+  }
+
+  const handleMonthsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHoldingMonths(e.target.value)
+  }
+
+  const DAYS_IN_COMMON_YEAR = 365
+  const DAYS_IN_LEAP_YEAR = 366
+  const MONTHS_IN_YEAR = 12
+  const CENTURY_YEAR_DIVISOR = 100
+  const CENTURY_LEAP_YEAR_DIVISOR = 400
+  const LEAP_YEAR_DIVISOR = 4
+  const PERCENTAGE_DIVISOR = 100
+  const REAL_MONTH_NUMBER_INDEX_INCREMENT = 1
+
+  const getTotalHoldingDays = (currentYear: number, months: number) => {
+    let totalDays = 0
+    for (let monthIndex = 0; monthIndex < months; monthIndex++) {
+      const yearOffset = Math.floor(monthIndex / MONTHS_IN_YEAR)
+      const year = currentYear + yearOffset
+      const monthOffset = monthIndex % MONTHS_IN_YEAR
+      const month = monthOffset + REAL_MONTH_NUMBER_INDEX_INCREMENT
+      const numberOfDaysInCurrentMonth = new Date(year, month, 0).getDate()
+      totalDays += numberOfDaysInCurrentMonth
+    }
+
+    return totalDays
+  }
+
+  const isLeapYear = (year: number) => {
+    return (
+      (year % LEAP_YEAR_DIVISOR === 0 && year % CENTURY_YEAR_DIVISOR !== 0) ||
+      year % CENTURY_LEAP_YEAR_DIVISOR === 0
+    )
+  }
+
+  const calculateInterest = () => {
+    if (amount && interestRate && taxRate && holdingMonths) {
+      console.log('inputs')
+      console.log(amount, interestRate, taxRate, holdingMonths)
+      const year = new Date().getFullYear()
+      const holdingDays = getTotalHoldingDays(year, parseInt(holdingMonths))
+      const daysInYear = isLeapYear(year) ? DAYS_IN_LEAP_YEAR : DAYS_IN_COMMON_YEAR
+
+      console.log('parsed inputs')
+      console.log(holdingDays, daysInYear)
+
+      const interest =
+        parseFloat(amount) *
+        (parseFloat(interestRate) / PERCENTAGE_DIVISOR) *
+        (holdingDays / daysInYear) *
+        (1 - parseFloat(taxRate) / PERCENTAGE_DIVISOR)
+
+      setInterest(interest.toFixed(2))
+    }
+  }
 
   return (
     <div className='card shadow-xl mt-8'>
       <div className='card-body'>
         <div className='mb-4'>
-          <div className='label'>
-            <span className='label-text'>Deposit Amount</span>
-          </div>
+          <label className='label'>
+            <span className='label-text'>Deposit Amount (IDR)</span>
+          </label>
           <input
             type='number'
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={handleAmountChange}
             className='input input-bordered w-full rounded'
-            placeholder='Enter amount'
+            placeholder='Enter amount (min 1,000,000 IDR)'
           />
         </div>
         <div className='mb-4 flex flex-row space-x-10'>
           <div className='flex-1'>
-            <div className='label'>
+            <label className='label'>
               <span className='label-text'>Annual Interest Rate (%)</span>
-            </div>
+            </label>
             <input
-              type='number'
-              value={interest}
-              onChange={(e) => setInterest(e.target.value)}
-              className='input input-bordered w-full border rounded'
+              type='text'
+              value={interestRate}
+              onChange={handleInterestRateChange}
+              className='input input-bordered w-full rounded'
               placeholder='Enter interest rate'
             />
           </div>
           <div className='flex-1'>
-            <div className='label'>
+            <label className='label'>
               <span className='label-text'>Tax Rate (%)</span>
-            </div>
+            </label>
             <input
-              type='number'
-              value={interest}
-              onChange={(e) => setInterest(e.target.value)}
-              className='input input-bordered w-full border rounded'
-              placeholder='Enter interest rate'
+              type='text'
+              value={taxRate}
+              onChange={handleTaxRateChange}
+              className='input input-bordered w-full rounded'
+              placeholder='Enter tax rate'
             />
           </div>
         </div>
         <div className='mb-6'>
-          <div className='label'>
-            <span className='label-text'>Number of Month</span>
-          </div>
+          <label className='label'>
+            <span className='label-text'>Number of Months</span>
+          </label>
           <input
             type='number'
-            value={years}
-            onChange={(e) => setYears(e.target.value)}
-            className='input input-bordered w-full border rounded'
-            placeholder='Enter number of years'
+            value={holdingMonths}
+            onChange={handleMonthsChange}
+            className='input input-bordered w-full rounded'
+            placeholder='Enter number of months (min 1 month)'
           />
         </div>
         <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Calculate</button>
+          <button onClick={calculateInterest} className='btn btn-primary'>
+            Calculate
+          </button>
         </div>
+        <div>Calculated interest : {interest}</div>
       </div>
     </div>
   )
--- src/components/DepositSection/DepositTypeSelector.tsx
+++ src/components/DepositSection/DepositTypeSelector.tsx
@@ -14,21 +14,21 @@ const DepositTypeSelector = () => {
       <div role='tablist' className='tabs tabs-lifted'>
         <button
           role='tab'
-          className={`tab ${selectedOption === DepositType.NONARO ? 'tab-active' : ''}`}
+          className={`tab ${selectedOption === DepositType.NONARO ? 'tab-active' : ''} font-bold`}
           onClick={() => setSelectedOption(DepositType.NONARO)}
         >
           {DepositType.NONARO}
         </button>
         <button
           role='tab'
-          className={`tab ${selectedOption === DepositType.ARO ? 'tab-active' : ''}`}
+          className={`tab ${selectedOption === DepositType.ARO ? 'tab-active' : ''} font-bold`}
           onClick={() => setSelectedOption(DepositType.ARO)}
         >
           {DepositType.ARO}
         </button>
         <button
           role='tab'
-          className={`tab ${selectedOption === DepositType.AROPLUS ? 'tab-active' : ''}`}
+          className={`tab ${selectedOption === DepositType.AROPLUS ? 'tab-active' : ''} font-bold`}
           onClick={() => setSelectedOption(DepositType.AROPLUS)}
         >
           {DepositType.AROPLUS}
@@ -37,18 +37,35 @@ const DepositTypeSelector = () => {
 
       <div className='p-10'>
         {selectedOption === DepositType.NONARO && (
-          <div>Your interest will be returned to you without reinvestment.</div>
+          <p>
+            <strong>Simple Interest:</strong> Get your interest paid out directly.
+            <br />
+            <br />
+            For example, on a IDR 10,000,000 deposit with a 5% annual interest rate, you'll receive
+            IDR 500,000 in interest.
+          </p>
         )}
         {selectedOption === DepositType.ARO && (
-          <div>
-            Automatic Roll-Over (ARO) allows your interest to be reinvested, maximizing growth.
-          </div>
+          <p>
+            <strong>Smart Saver:</strong> Watch your savings grow automatically as interest is added
+            to your savings account and your deposit continues to be reinvested.
+            <br />
+            <br />
+            For example, on a IDR 10,000,000 deposit with a 5% annual interest rate, IDR 500,000 in
+            interest will be added to your savings account, and the IDR 10,000,000 deposit will
+            continue to earn interest.
+          </p>
         )}
         {selectedOption === DepositType.AROPLUS && (
-          <div>
-            ARO with Compounding ensures faster growth through reinvestment and compounding
-            interest.
-          </div>
+          <p>
+            <strong>Maximize Your Returns:</strong> Enjoy the power of compounding interest as your
+            savings grow exponentially.
+            <br />
+            <br />
+            For example, on a IDR 10,000,000 deposit with a 5% annual interest rate, IDR 500,000 in
+            interest will be added to your savings account, and the new total (IDR 10,500,000) will
+            continue to earn interest in the next period.
+          </p>
         )}
       </div>
     </div>
