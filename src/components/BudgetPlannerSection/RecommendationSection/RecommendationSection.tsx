973e7b45f6786a716ee8e533a27d65367d962039
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2025-03-31 18:00:16 +0700

    Feat budget-planner-feature : Add download functionality to the planned budget

--- src/components/BudgetPlannerSection/RecommendationSection/RecommendationSection.tsx
+++ src/components/BudgetPlannerSection/RecommendationSection/RecommendationSection.tsx
@@ -5,13 +5,22 @@ interface RecommendationSectionProps {
 const RecommendationSection = (props: RecommendationSectionProps) => {
   const { recommendation } = props
 
-  // Determine if the recommendation is positive or negative
-  const isPositive = recommendation.toLowerCase().includes('have') // Example logic to check positivity
+  const isPositive = recommendation.toLowerCase().includes('have')
+
+  const handleExportToTxt = () => {
+    const element = document.createElement('a')
+    const file = new Blob([recommendation], { type: 'text/plain' })
+    element.href = URL.createObjectURL(file)
+    element.download = 'recommendation.txt'
+    document.body.appendChild(element)
+    element.click()
+    document.body.removeChild(element)
+  }
 
   return (
     <div className='mt-8'>
       <div
-        className={`alert shadow-lg whitespace-pre-line p-6 rounded-lg ${
+        className={`flex justify-between alert shadow-lg whitespace-pre-line p-6 rounded-lg ${
           isPositive
             ? 'bg-green-100 border-green-500 text-green-800'
             : 'bg-red-100 border-red-500 text-red-800'
@@ -49,6 +58,9 @@ const RecommendationSection = (props: RecommendationSectionProps) => {
           </div>
           <span className='text-lg text-left font-medium'>{recommendation}</span>
         </div>
+        <button onClick={handleExportToTxt} className='mt-4 btn btn-primary text-sm text-[#ffffff]'>
+          Download
+        </button>
       </div>
     </div>
   )
